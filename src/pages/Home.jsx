e29899a67439fd06737d504ee014d755c0b7efb8
Hanna Mazur <hanna2mazur@gmail.com>
2022-04-06 06:07:18 +0200

    Minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Header from "../components/Header";
-import Services from "../components/Services";
+import Services from "../components/services";
 
 function Home() {
   return (
