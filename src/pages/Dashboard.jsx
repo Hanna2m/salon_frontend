9059a867824dea2ef55f850874d3b30157d48cec
Rachael Kunz <rachaelmkunz@gmail.com>
2022-04-10 14:01:25 +0200

    Add adminCalendar to show appointments

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
-
+import AdminCalendar from "../components/AdminCalendar";
 
 import Header from "../components/Header";
 
 const Dashboard = () => {
-    return(
-        <>
+  return (
+    <>
       <Header />
-        <h2>Dashboard</h2>;
-        <Link to="/services">Services</Link>;
-        <Link to="/customers">Customers</Link>
-        <Outlet />
-        </>
-        
-    )
-}
+      <h2>Dashboard</h2>;<Link to="/services">Services</Link>;
+      <Link to="/customers">Customers</Link>
+      <AdminCalendar />
+      <Outlet />
+    </>
+  );
+};
 
 export default Dashboard;
