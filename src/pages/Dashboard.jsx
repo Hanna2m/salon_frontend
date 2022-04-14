a42634f53492273e48dd4ed252dd2beb14a9d773
Rachael Kunz <rachaelmkunz@gmail.com>
2022-04-14 11:20:34 +0200

    Dashboard styling

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,21 +3,35 @@ import { Link, Outlet, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import AdminCalendar from "../components/AdminCalendar";
 import AuthContext from "../context/AuthProvider";
-
 import Header from "../components/Header";
+import "../components/styles/_dashboard.css";
 
 const Dashboard = () => {
   const navigate = useNavigate();
   return (
-    <>
+    <section className="dashboard">
       <Header />
-      <div className="content">
-        <h2>Dashboard</h2>;<Link to="/services">Services</Link>;
-        <Link to="/customers">Customers</Link>
-        <AdminCalendar />
+      <div className="content dashboard-content">
+        <h2 className="dashboard-title">Dashboard</h2>
+        <div className="dashboard-card-container">
+          <div>
+            <Link className="dashboard-card" to="/services">
+              Services
+            </Link>
+          </div>
+          <div>
+            <Link className="dashboard-card" to="/customers">
+              Customers
+            </Link>
+          </div>
+        </div>
+        <div className="admin-cal">
+          <h3>Appointments for today:</h3>
+          <AdminCalendar />
+        </div>
         <Outlet />
       </div>
-    </>
+    </section>
   );
 };
 
